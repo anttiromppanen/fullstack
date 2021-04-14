4cd5fb610c28f10445db0b86fbdcc9ad09410eb9
Antti Romppanen <romppanen.antti@gmail.com>
2021-04-14 11:22:12 +0300

    7.1 - 7.3

---
+++ osa7/routed-anecdotes/src/index.js
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+)
