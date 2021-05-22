ec944eaf0d2498a009ed4b893c03aa9c9d7d6bdc
Antti Romppanen <romppanen.antti@gmail.com>
2021-05-22 12:41:49 +0300

    9.12 - 9.13

--- osa9/patientor_backend/src/routes/patients.ts
+++ osa9/patientor_backend/src/routes/patients.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from 'express';
 
 const router = express.Router();
@@ -9,7 +11,20 @@ router.get('/', (_req, res) => {
 });
 
 router.post('/', (_req, res) => {
-  
+  try {
+    const { name, ssn, dateOfBirth, occupation, gender } = _req.body;
+    const newPatient = patientsService.addPatient({
+      name,
+      ssn,
+      dateOfBirth,
+      occupation,
+      gender
+    });
+
+    res.json(newPatient);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 });
 
 export default router;
