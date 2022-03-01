c0fed0e03c3a62d5b377a14e5934b126a1a38d68
Antti Romppanen <romppanen.antti@gmail.com>
2022-03-02 01:05:51 +0200

    api url changed

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -1,28 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import './App.css';
-import ShowCountries from './components/ShowCountries'
-import Filter from './components/Filter'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import ShowCountries from "./components/ShowCountries";
+import Filter from "./components/Filter";
 
 const App = () => {
-  const [ countries, setCountries ] = useState([])
-  const [ filterValue, setFilterValue ] = useState('')
+  const [countries, setCountries] = useState([]);
+  const [filterValue, setFilterValue] = useState("");
 
   useEffect(() => {
     axios
-      .get('https://restcountries.eu/rest/v2/all')
-      .then(res => setCountries(res.data))
-  }, [])
+      .get("https://restcountries.com/v2/all")
+      .then((res) => setCountries(res.data));
+  }, []);
 
-  const filteredCountries = 
-    countries.filter(x => x.name.toLowerCase().includes(filterValue.toLowerCase()))
+  const filteredCountries = countries.filter((x) =>
+    x.name.toLowerCase().includes(filterValue.toLowerCase())
+  );
 
   return (
     <div>
-      <Filter filterValue={ filterValue } setFilterValue={ setFilterValue } />
-      <ShowCountries countries={ filteredCountries } setFilterValue={ setFilterValue } />
+      <Filter filterValue={filterValue} setFilterValue={setFilterValue} />
+      <ShowCountries
+        countries={filteredCountries}
+        setFilterValue={setFilterValue}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
