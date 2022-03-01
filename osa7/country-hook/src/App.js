7abb18e6d5f753ad62473a969ad1874a1d596b51
Antti Romppanen <romppanen.antti@gmail.com>
2022-03-02 00:38:52 +0200

    api url was changed

--- osa7/country-hook/src/App.js
+++ osa7/country-hook/src/App.js
@@ -1,84 +1,84 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const useField = (type) => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState("");
 
   const onChange = (event) => {
-    setValue(event.target.value)
-  }
+    setValue(event.target.value);
+  };
 
   return {
     type,
     value,
-    onChange
-  }
-}
+    onChange,
+  };
+};
 
 const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
-  const [found, setFound] = useState(false)
+  const [country, setCountry] = useState(null);
+  const [found, setFound] = useState(false);
 
   useEffect(() => {
-      axios
-        .get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
-        .then((res) => {
-          setCountry(res.data[0])
-          setFound(true)
-        })
-        .catch((error) => {
-          setCountry('placeholder')
-          setFound(null)
-          console.log(error.message)
-        })
-  }, [name])
- 
+    axios
+      .get(`https://restcountries.com/v2/name/${name}?fullText=true`)
+      .then((res) => {
+        setCountry(res.data[0]);
+        setFound(true);
+      })
+      .catch((error) => {
+        setCountry("placeholder");
+        setFound(null);
+        console.log(error.message);
+      });
+  }, [name]);
+
   if (!country) {
-    return false
-  }  
+    return false;
+  }
 
   if (!found) {
-    return country
+    return country;
   }
 
   return {
     data: country,
-    found
-  }
-}
+    found,
+  };
+};
 
 const Country = ({ country }) => {
   if (!country) {
-    return null
+    return null;
   }
 
   if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
+    return <div>not found...</div>;
   }
 
   return (
     <div>
       <h3>{country.data.name} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img
+        src={country.data.flag}
+        height="100"
+        alt={`flag of ${country.data.name}`}
+      />
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
-  const nameInput = useField('text')
-  const [name, setName] = useState('')
-  const country = useCountry(name)
+  const nameInput = useField("text");
+  const [name, setName] = useState("");
+  const country = useCountry(name);
 
   const fetch = (e) => {
-    e.preventDefault()
-    setName(nameInput.value)
-  }
+    e.preventDefault();
+    setName(nameInput.value);
+  };
 
   return (
     <div>
@@ -89,7 +89,7 @@ const App = () => {
 
       <Country country={country} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
